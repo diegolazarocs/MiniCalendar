d7ec4b370cbba1555ef8234263b8a3adb896b934
Diego Lázaro <111025421+diegolazarocs@users.noreply.github.com>
2024-10-03 22:19:10 -0300

    Update scripts.js

--- MiniCalendar/scripts.js
+++ MiniCalendar/scripts.js
@@ -1,28 +1,15 @@
-const date = document.getElementById("date");
-const day = document.getElementById("day");
-const mouth = document.getElementById("month");
-const year = document.getElementById("year");
+const monthNameEl = document.getElementById("month-name");
+const dayNameEl = document.getElementById("day-name");
+const dayNumEl = document.getElementById("day-number");
+const yearEl = document.getElementById("year");
 
-const today = new Date();
-
-const weekDays = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta"];
-
-const allMonths = [
-  "Janeiro",
-  "Fevereiro",
-  "Março",
-  "Abril",
-  "Maio",
-  "Junho",
-  "Julho",
-  "Agosto",
-  "Setembro",
-  "Outubro",
-  "Novembro",
-  "Dezembro",
-];
-
-date.innerHTML = (today.getDate() < 10 ? "0" : "") + today.getDate();
-day.innerHTML = weekDays[today.getDay()];
-mouth.innerHTML = allMonths[today.getMonth()];
-year.innerHTML = today.getFullYear();
+const date = new Date();
+const month = date.getMonth();
+monthNameEl.innerText = date.toLocaleString("pt", {
+  month: "long",
+});
+dayNameEl.innerText = date.toLocaleDateString("pt", {
+  weekday: "long",
+});
+dayNumEl.innerText = date.getDate();
+yearEl.innerText = date.getFullYear();
